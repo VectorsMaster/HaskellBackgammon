707d54b87da176ee8187b85e9823738f7efdba1a
VectorsMaster <ahmadhasansarhana@gmail.com>
2023-11-09 01:14:04 +0300

    implement all rules of the game

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,7 @@ module Main where
 
 import MyLib
 import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game ( Event )
-
-
+import Graphics.Gloss.Interface.Pure.Game
 
 
 -- | Represents a sequence of moves (in their order) made by ONE PLAYER in ONE TURN
@@ -20,42 +18,87 @@ type Move = [(Int, Int)]
 -- | The Second integer represents the number of pieces in this triangle from it can be from 0 to 15
 -- | The last integer represents "mahbousa" it can be 1 if this triangle contains one piece of the other color which cannot move and 0 otherwise
 type GameTriangle = (Int, Int, Int)
+data State = ChooseSteps | Moving
+    deriving (Eq, Show)
 
 -- | the dirction of triangles and pieces
 data Direction = Upward | Downward
 
 -- | dices : the values of the dices in the current turn
 -- | turn  : can be either 0 or 1
--- | triangles : the state of triangles according to the player who will play in this turn. it has exactly 24 elements.
+-- | triangles : the state of triangAvailableles according to the player who will play in this turn. it has exactly 24 elements.
 -- | availableMoves : all valid sequence of moves that the player can play in this turn according to state of the game.
 data World = World {
     dices :: (Int, Int),
     turn :: Int,
     triangles :: [GameTriangle],
-    availableMoves :: [Move],
+    availableMoves :: [Int],
     randoms :: [Int],
-    bearingOff :: Bool
-
+    bearingOff :: Bool,
+    state :: State,
+    finished :: Bool,
+    choosedSteps :: Int
 } deriving (Eq, Show)
 
-main :: IO ()
-main = display
-    (InWindow "Backgammon" (700, 650) (10, 10))
+backGammon :: IO ()
+backGammon = play
+    (InWindow "Backgammon" (1000, 1000) (100, 100))
     white
-    (drawBoard initialWorld)
+    100
+    (throwDices initialWorld)
+    drawBoard
+    handleInput
+    (\_ world -> world)
 
--- main = play
---     (InWindow "Backgammon" (1000, 1000) (100, 100))
---     white
---     100
---     initialWorld
---     drawBoard
---     handleInput
---     (\_ world -> world)
+main :: IO () 
+main = do 
+    print (dices $ throwDices initialWorld)
+    print (state $ throwDices initialWorld)
+    
+-- | A function to convert keypress event to integer
+convEvent :: Event -> Int
+convEvent (EventKey (Char c) Down _ _)
+    | not (MyLib.isLetterOrDigit c) = 24
+    | otherwise               = MyLib.charToInt c
+convEvent _ = 24
 
 -- | Change the state of the game according to user's input
--- handleInput :: Event -> World -> World
--- handleInput event world = _toDo
+handleInput :: Event -> World -> World
+handleInput event world
+    | myEvent > 23 = world
+    | state world == ChooseSteps = chooseSteps myEvent world
+    | otherwise = tryMovePiece (myEvent, choosedSteps world) world
+        where
+            myEvent = convEvent event
+
+-- | A function to choose the number of steps a piece will move in this turn
+chooseSteps :: Int -> World -> World
+chooseSteps x (World dc t tr av r b st f _) = if x <= 6 then World dc t tr av r b st f 0 else World dc t tr av r b Moving f x
+
+-- | A function to perform a move 
+tryMovePiece :: (Int, Int) -> World -> World
+tryMovePiece (id, steps) world
+    | not (exists steps (availableMoves world)) = world
+    | otherwise = ret
+        where
+            (_, newState) = isValidMove (id, steps) world
+            (World dc t tr av r b st _ _) = newState
+            newAv = MyLib.removeOneOccurence steps av
+            ret
+              | gameFinished newState = World dc t tr av r b st True (-1)
+              | null newAv = flipTurn newState
+              | otherwise = updateBearingOff (World dc t tr newAv r b ChooseSteps False (-1))
+
+-- | A function to determine if a game is finished by counting the number of pieces owned by this turn player
+gameFinished :: World -> Bool
+gameFinished world = countInTriangles (triangles world)
+    where
+        countInTriangles [] = True
+        countInTriangles ((x, y, z):xs)
+            | y == 0 = countInTriangles xs
+            | x == turn world = False
+            | z == 1 = False
+            | otherwise = countInTriangles xs
 
 initialTriangles :: [(Int, Int, Int)]
 initialTriangles = [
@@ -64,7 +107,7 @@ initialTriangles = [
     ]
 
 initialWorld :: World
-initialWorld = World (1, 6) 0 initialTriangles [] MyLib.myArray False
+initialWorld = World (-1, -1) 0 initialTriangles [] MyLib.myArray False ChooseSteps False (-1)
 
 brown :: Color
 brown = makeColor 0.4375 0.30859375 0.1640625 1.0
@@ -85,17 +128,14 @@ drawBoard world = translate (-330) 220 (dice <> table <> downwardTriangles <> sh
         table = translate (50 * fromIntegral quarter + 10) (-height/2) (Color brown $ rectangleSolid (50*(fromIntegral quarter * 2) + break) 500)
 
         half1Downwards = drawGameTriangles (take quarter (triangles world)) Downward
-        half1Upwards = drawGameTriangles (takeRange (quarter * 2) (quarter * 3 - 1) (triangles world)) Upward
+        half1Upwards = drawGameTriangles (MyLib.takeRange (quarter * 2) (quarter * 3 - 1) (triangles world)) Upward
 
-        half2Downwards = drawGameTriangles (takeRange quarter (quarter * 2 - 1) (triangles world)) Downward
-        half2Upwards = drawGameTriangles (takeRange (quarter * 3) len (triangles world)) Upward
+        half2Downwards = drawGameTriangles (MyLib.takeRange quarter (quarter * 2 - 1) (triangles world)) Downward
+        half2Upwards = drawGameTriangles (MyLib.takeRange (quarter * 3) len (triangles world)) Upward
 
         shiftedUpwardTriangles = translate (50*(fromIntegral quarter * 2 - 1) + break) (-height) upwardTriangles
         currentDice = dices world
-        dice = if getFirstElement currentDice /= (-1) then Text (show currentDice) else blank
-
-getFirstElement :: (a, b) -> a
-getFirstElement (x, _) = x
+        dice = if fst currentDice /= (-1) then Text (show currentDice) else blank
 
 
 -- Draw game triangles
@@ -140,9 +180,6 @@ drawGameTriangle triangle direction = directedTriangle <> circles
             Downward -> Polygon [(0,0), (25,-100), (50,0)]
         circles = drawPieces triangle direction
 
-takeRange :: Int -> Int -> [a] -> [a]
-takeRange i j l = take (j-i+1) (drop i l)
-
 colorExist :: GameTriangle -> Int -> Bool
 colorExist (x, y, _) clr = x == clr && y > 0
 
@@ -166,7 +203,7 @@ changeTriangles lst id val trn = iter lst val id trn 0
 
 -- | removing or adding number of pieces to a specific triangle in the world
 changeWorld :: World -> Int -> Int -> World
-changeWorld (World d trn t a r b) id val = World d trn (changeTriangles t id val trn) a r b
+changeWorld (World d trn t a r b st f ch) id val = World d trn (changeTriangles t id val trn) a r b st f ch
 
 fndColor :: GameTriangle -> Int
 fndColor (x, _, _) = x
@@ -186,7 +223,8 @@ allInHalf lst trn = iter lst 0
         iter (x:xs) pos = not (pos >= 12 && check x) && iter xs (pos + 1)
 
 updateBearingOff :: World -> World
-updateBearingOff (World d trn t a r b) = if allInHalf t trn then World d trn t a r True else World d trn t a r False
+updateBearingOff (World dc t tr av r b st f ch) = 
+    if allInHalf tr t then World dc t tr av r True st f ch else World dc t tr av r False st f ch
 
 -- | First parameter (x, y)
 -- | X: Represents the index of the triangle from where the character to be moved is chosen.
@@ -209,33 +247,27 @@ isValidMove (x, y) world
             desCount = fndCount desTriangle
             desColor = fndColor desTriangle
 
+-- | flip the turn
+flipTurn :: World -> World
+flipTurn (World _ t tr _ r _ _ _ _) = throwDices newWorld
+    where
+        revTurn = inv t
+        revTrgs = reverse tr
+        newWorld = World (-1, -1) revTurn revTrgs [] r False ChooseSteps False (-1)
 
--- | Checks if a sequence of moves to be played by one player in this turn is valid or not using isValidMove function.
-isValidMovesSequence :: Move -> World -> (Bool, World)
-isValidMovesSequence [] world = (True, world)
-isValidMovesSequence lst world = iter lst world 
-    where 
-        iter [] curWorld = (True, curWorld)
-        iter (x:xs) curWorld 
-            | ok        = iter xs nextWorld
-            | otherwise = (False, curWorld)
-                where 
-                    (ok, newWorld) = isValidMove x world
-                    nextWorld = updateBearingOff newWorld
+-- | A function to check if there exist a valid triangle where you pick a piece and move it a given number of steps
+existValidTriangle :: World -> Int -> Bool
+existValidTriangle world steps = any isValid moves
+    where
+        moves = map (\a -> (a, steps)) [0..23]
+        isValid move = fst (isValidMove move world)
 
--- | Returns a list of all moves according to the numbers in the dices (valid and not valid)
--- getAllMoves :: World -> [Move]
--- getAllMoves = _toDo
-
--- -- | filter all Moves resulted from getAllMoves and assign the valid of them to the availableMoves member in World Struct
--- getValidMoves :: World -> World
--- getValidMoves = _toDo
-
-
-handleState :: Move -> World -> World
-handleState move world = world
-
--- -- | generate to random numbers from 1 to 6 and assign them the dices member in World struct
+-- | generate to random numbers from 1 to 6 and assign them the dices member in World struct
 throwDices :: World -> World
-throwDices (World (-1, -1) turn triangles availableMoves (r1:(r2:rs)) b) = World (r1, r2) turn triangles availableMoves rs b
+throwDices (World (-1, -1) t tr _ (r1:(r2:rs)) _ _ _ _) = updateBearingOff nextState
+    where
+        newWorld = World (r1, r2) t tr [] rs False ChooseSteps False (-1)
+        moves = if r1 == r2 then [r1, r1, r1, r1] else [r1, r2]
+        newAv = filter (existValidTriangle newWorld) moves
+        nextState = World (r1, r2) t tr newAv rs False ChooseSteps False (-1)
 throwDices world = world
