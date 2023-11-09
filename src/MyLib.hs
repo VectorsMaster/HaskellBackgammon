2d9b75a344598c87853518ecf3b556d6bf336607
VectorsMaster <ahmadhasansarhana@gmail.com>
2023-11-09 18:43:16 +0300

    fix bugs & add instructions

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -3,9 +3,14 @@ module MyLib where
 
 import System.Random
 import Data.Char (isDigit)
+import GHC.IO (unsafePerformIO)
 
 myArray :: [Int]
-myArray = randomRs (1, 6) (mkStdGen 3)
+myArray = drop 1000 $ randomRs (1, 6) (mkStdGen seed)
+    where
+        seed = mrg (unsafePerformIO randomIO)
+        mrg x = x `mod` 6 + 1
+    
 
 -- | function to check if element exists in array of integers 
 exists :: Int -> [Int] -> Bool
@@ -20,8 +25,12 @@ removeOneOccurence val (x:xs) = if val == x then xs else x : removeOneOccurence 
 isLetterOrDigit :: Char -> Bool
 isLetterOrDigit c = (c >= 'a' || c <= 'x') || (c >= '1' && c<='6')
 
-charToInt :: Char -> Int 
-charToInt c = if isDigit c then fromEnum c - fromEnum '0' else fromEnum c - fromEnum 'a' 
+charToInt :: Char -> Int
+charToInt c = if isDigit c then fromEnum c - fromEnum '0' else fromEnum c - fromEnum 'a'
 
 takeRange :: Int -> Int -> [a] -> [a]
 takeRange i j l = take (j-i+1) (drop i l)
+
+inv :: Int -> Int
+inv 0 = 1
+inv _ = 0
